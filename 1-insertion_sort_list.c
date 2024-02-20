a25eb427d338484bb26427b3c99aaad6e4e45f44
Adobea <144677928+AdobeaAfia@users.noreply.github.com>
2024-02-20 11:22:41 +0000

    sorting_algorithms

--- 1-insertion_sort_list.c
+++ 1-insertion_sort_list.c
@@ -1,48 +1,43 @@
 #include "sort.h"
 
 /**
- * swap_nodes - Swap two nodes in a listint_t doubly-linked list.
- * @h: A pointer to the head of the doubly-linked list.
- * @n1: A pointer to the first node to swap.
- * @n2: The second node to swap.
- */
-void swap_nodes(listint_t **h, listint_t **n1, listint_t *n2)
-{
-	(*n1)->next = n2->next;
-	if (n2->next != NULL)
-		n2->next->prev = *n1;
-	n2->prev = (*n1)->prev;
-	n2->next = *n1;
-	if ((*n1)->prev != NULL)
-		(*n1)->prev->next = n2;
-	else
-		*h = n2;
-	(*n1)->prev = n2;
-	*n1 = n2->prev;
-}
+* insertion_sort_list - sorts a doubly linked list in ascending order
+* using the Insertion sort algorithm
+* @list: double pointer to the head of the list
+*/
 
-/**
- * insertion_sort_list - Sorts a doubly linked list of integers
- *                       using the insertion sort algorithm.
- * @list: A pointer to the head of a doubly-linked list of integers.
- *
- * Description: Prints the list after each swap.
- */
 void insertion_sort_list(listint_t **list)
 {
-	listint_t *iter, *insert, *tmp;
+	listint_t *current, *prev, *next;
 
 	if (list == NULL || *list == NULL || (*list)->next == NULL)
 		return;
 
-	for (iter = (*list)->next; iter != NULL; iter = tmp)
+	current = (*list)->next;
+	while (current != NULL)
 	{
-		tmp = iter->next;
-		insert = iter->prev;
-		while (insert != NULL && iter->n < insert->n)
+		next = current->next;
+		prev = current->prev;
+
+		while (prev != NULL && prev->n > current->n)
 		{
-			swap_nodes(list, &insert, iter);
-			print_list((const listint_t *)*list);
+			if (prev->prev != NULL)
+				prev->prev->next = current;
+			else
+				*list = current;
+
+			if (current->next != NULL)
+				current->next->prev = prev;
+
+			prev->next = current->next;
+			current->prev = prev->prev;
+			current->next = prev;
+			prev->prev = current;
+
+			print_list(*list);
+			prev = current->prev;
 		}
+
+		current = next;
 	}
 }
